122caf9ed4e11f6c7764ded1882faac4aaf41a49
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-01-21 20:10:38 +0900

    [level 3] Title: 사라지는 발판, Time: 347.84 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.py
@@ -1,50 +1,58 @@
-dir = [(1,0),(0,1),(-1,0),(0,-1)]
-def get_next_loc(board, loc):
-    R, C = len(board), len(board[0])
-    cur_r, cur_c = loc[0], loc[1]
-    possible_loc = []
-    for dr, dc in dir:
-        nr, nc = cur_r + dr, cur_c + dc
-        if 0 <= nr < R and 0 <= nc < C and board[nr][nc]:
-            possible_loc.append([nr, nc])
-    return possible_loc
-        
-def dfs(result, board, aloc, bloc):
-    if result % 2 == 0 : # A 이동
-        next_loc = get_next_loc(board, aloc)
-    else : # B 이동
-        next_loc = get_next_loc(board, bloc)
-    if not next_loc :
-        return result % 2 != 0, result # A 승리 시 True, B 승리 시 False
-    if aloc == bloc :
-        return (result+1) % 2 != 0, result + 1 # A 승리 시 True, B 승리 시 False
-    win, lose = [], []
-    if result % 2 == 0: # A 이동 차례
-        board[aloc[0]][aloc[1]] = 0
-        for nr, nc in next_loc:
-            a_win, cnt = dfs(result + 1, board, [nr, nc], bloc)
-            if a_win:
-                win.append(cnt)
-            else:
-                lose.append(cnt)
-        board[aloc[0]][aloc[1]] = 1
-    else: # B 이동
-        board[bloc[0]][bloc[1]] = 0
-        for nr, nc in next_loc:
-            a_win, cnt = dfs(result+1, board, aloc, [nr, nc])
-            if not a_win:
-                win.append(cnt)
-            else:
-                lose.append(cnt)
-        board[bloc[0]][bloc[1]] = 1
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+INF = 987654321
 
-    if win:                             # 승리하는 경우가 하나라도 있으면 
-        return result % 2 == 0, min(win)  # 승리하는 사람과 가장 빨리 승리하는 경우를 반환한다.
-    else:                               # 승리하는 경우가 하나도 없으면
-        return result % 2 != 0, max(lose) # 패배하는 가장 긴 루트를 반환한다.
 
-            
-    
 def solution(board, aloc, bloc):
-    winner, answer = dfs(0, board, aloc, bloc)
-    return answer
+    return solve(board, aloc[0], aloc[1], bloc[0], bloc[1])[1]
+
+
+def in_range(board, y, x):
+    if y < 0 or y >= len(board) or x < 0 or x >= len(board[0]):
+        return False
+    return True
+
+
+def is_finished(board, y, x):
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if in_range(board, ny, nx) and board[ny][nx]:
+            return False
+    return True
+
+
+def solve(board, y1, x1, y2, x2):
+    # can_win, turn
+    if is_finished(board, y1, x1):
+        return [False, 0]
+
+    # 서로 두 위치가 같을 때 이번 턴에 움직이면 무조건 이기므로
+    if y1 == y2 and x1 == x2:
+        return [True, 1]
+
+    min_turn = INF
+    max_turn = 0
+    can_win = False
+
+    # dfs
+    for i in range(4):
+        ny = y1 + dy[i]
+        nx = x1 + dx[i]
+        if not in_range(board, ny, nx) or not board[ny][nx]:
+            continue
+
+        board[y1][x1] = 0
+        result = solve(board, y2, x2, ny, nx)  # 차례가 바뀌기 때문에 위치를 바꿔준다.
+        board[y2][x2] = 1
+
+        # 이 시점에서는 result[0]이 False여야만 현재 턴에서 내가 이길 수 있다.
+        if not result[0]:
+            can_win = True
+            min_turn = min(min_turn, result[1])
+        elif not can_win:
+            max_turn = max(max_turn, result[1])
+
+    turn = min_turn if can_win else max_turn
+
+    return [can_win, turn + 1]
