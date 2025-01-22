72491357a2601d5f6f9bb7e2f2b91f96d248f4e9
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-01-22 17:18:12 +0900

    [level 2] Title: 양궁대회, Time: 4.82 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -1,22 +1,49 @@
-from itertools import combinations_with_replacement
-
 def solution(n, info):
+
+    def brut(n, lion, idx): # 남은 화살수, 과녁, 현재 idx
+        nonlocal gap, answer
+
+        if idx > 10: # 정지조건
+            # 화살이 남은경우 마지막에 다쏜다고 가정
+            if n > 0:
+                lion[10] += n
+            score_apeach = 0
+            score_lion = 0
+            # 점수 세기
+            for idx in range(0,11):
+                if lion[idx] > apeach[idx]:
+                    score_lion += 10-idx
+                elif apeach[idx]:
+                    score_apeach += 10-idx
+
+            if score_lion - score_apeach > gap:
+                gap = score_lion - score_apeach
+                answer = lion[:]
+
+            # 낮은 점수를 많이 쏜 경우 정답 최신화
+            elif score_lion - score_apeach == gap:
+                for i in range(10,-1,-1):
+                    if answer[i] < lion[i]:
+                        answer = lion[:]
+                        break
+                    elif answer[i] > lion[i]:
+                        break
+            lion[10] -= n
+            return
+
+        # 이번 idx(점수) 어피치를 이기는 경우와 지는경우 모두 탐색
+        ## 이기는 경우
+        if apeach[idx] + 1 <= n:
+            lion[idx] = apeach[idx] + 1
+            brut(n-apeach[idx]-1, lion, idx+1)
+            lion[idx] = 0
+        ## 지는 경우
+        brut(n, lion, idx+1)
+
+    apeach = info
+    lion = [0] * 11
+    gap = 1
     answer = []
-    for cwr in combinations_with_replacement([i for i in range(11)], n):
-        apeach_info = [0]*11
-        for s in cwr:
-            apeach_info[10-s] += 1
-        lion_score, apeach_score = 0, 0
-        for i in range(11):
-            if info[i] > apeach_info[i]: #Lion이 더 많이 맞추면
-                lion_score += 10-i
-            elif info[i] < apeach_info[i]: #Apeach가 더 많이 맞추면
-                apeach_score += 10-i
-            elif info[i] and info[i] == apeach_info[i]: #동일하게 맞추고 1개 이상 맞추면
-                lion_score += 10-i
-        if lion_score < apeach_score:
-            answer.append([apeach_score-lion_score, apeach_info])
-    if not answer :
-         return [-1]
-    answer.sort(key = lambda x : -x[0])
-    return answer[0][1]
+    brut(n, lion, 0)
+
+    return answer if answer else [-1]
