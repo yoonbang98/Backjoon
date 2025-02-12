052ad73b030b975271701f008ad78449763f711c
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-02-12 09:59:14 +0900

    [level 4] Title: [카카오 인턴] 동굴 탐험, Time: 296.14 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/4/67260. ［카카오 인턴］ 동굴 탐험/［카카오 인턴］ 동굴 탐험.py
+++ 프로그래머스/4/67260. ［카카오 인턴］ 동굴 탐험/［카카오 인턴］ 동굴 탐험.py
@@ -1,29 +1,23 @@
+def dfs(graph, path, cur, visited):
+    if len(path) == len(visited):
+        return path
+    print(path)
+    for nei in graph[cur]:
+        if not visited[nei]:
+            visited[nei] = True
+            dfs(graph, path + [nei], nei, visited)
+            visited[nei] = False
+    return [-1]
+
 def solution(n, path, order):
     answer = True
     graph = [[] for _ in range(n+1)]
     for src, dst in path:
         graph[src].append(dst)
         graph[dst].append(src)
-    orders = [0]*n
-    for pre, nxt in order:
-        orders[nxt] = pre
-    visited = [0]*n
-    queue = [0]
-    num_visit = 0
-    
-    after = {}
-    while queue :
-        cur = queue.pop()
-        if orders[cur] and not visited[orders[cur]]:
-            after[orders[cur]] = cur
-            continue
-        visited[cur] = 1
-        num_visit += 1
-        
-        for nei in graph[cur]:
-            if not visited[nei]:
-                queue.append(nei)
-        if cur in after:
-            queue.append(after[cur])
-
-    return n == num_visit
+    print(graph)
+    visited = [False]*(n)
+    visited[0] = True
+    path = dfs(graph, [0], 0, visited)
+    print(path)
+    return answer
