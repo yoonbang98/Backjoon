3969e4b2d70edc057d03cc69a5203c5f9d65460f
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-03-13 21:26:18 +0900

    [level 3] Title: 양과 늑대, Time: 26.20 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,36 +1,33 @@
-
-
-
+    
 def solution(info, edges):
-    def dfs(sheep, wolf):
-        nonlocal answer
-        if sheep == wolf :
-            answer = max(answer, sheep)
-            return 
-        for n in range(N):
-            if visited[n]:
-                for nei in graph[n]:
-                    if not visited[nei]:
-                        if info[nei] == 0 : #양
-                            visited[nei] = True
-                            dfs(sheep + 1, wolf)
-                            visited[nei] = False
-                        else : #늑대
-                            visited[nei] = True
-                            dfs(sheep, wolf + 1)
-                            visited[nei] = False
-        answer = max(answer, sheep)
-        return
+    answer = 1
     N = len(info)
     graph = [[] for _ in range(N)]
-    visited = [False]*N
+
     for src, dst in edges:
         graph[src].append(dst)
         graph[dst].append(src)
     
+    def dfs(sheep, visited, wolf):
+        nonlocal answer
+        if sheep <= wolf:
+            answer = max(answer, sheep)
+            return
+        answer = max(answer, sheep)
+        for n in range(N):
+            if visited[n]:
+                for nei in graph[n]:
+                    if not visited[nei] and info[nei]: #늑대
+                        visited[nei] = True
+                        dfs(sheep, visited, wolf + 1)
+                        visited[nei] = False
+                    elif not visited[nei] and not info[nei] : #양
+                        visited[nei] = True
+                        dfs(sheep + 1, visited, wolf)
+                        visited[nei] = False
+        return
+    visited = [False]*N
     visited[0] = True
-    answer = 1
-    dfs(1,0)
-    
+    dfs(1, visited, 0)
     
     return answer
