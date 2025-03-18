9283e9d47e9c44a85a8c066d10f58f6829259a56
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-03-18 11:55:36 +0900

    [level 4] Title: 호텔 방 배정, Time: 520.36 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
@@ -7,10 +7,11 @@ def solution(k, room_number):
         chk_in = find_emptyroom(num,rooms)
     return list(rooms.keys())
 
-def find_emptyroom(chk, rooms): # 재귀함수
-    if chk not in rooms.keys(): # 빈 방이면
-        rooms[chk] = chk+1 # rooms에 새 노드 추가
-        return chk # 요청한 방
-    empty = find_emptyroom(rooms[chk], rooms) # 재귀함수 호출
-    rooms[chk] = empty+1 # (배정된 방+1)을 부모노드로 변경
-    return empty # 새로 찾은 빈 방
+def find_emptyroom(chk, rooms):
+    if chk not in rooms.keys():
+        rooms[chk] = chk + 1
+        return chk
+    empty = find_emptyroom(rooms[chk], rooms)
+    rooms[chk] = empty + 1
+    return empty
+    
