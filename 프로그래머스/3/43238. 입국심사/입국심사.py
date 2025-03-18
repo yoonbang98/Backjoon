5646084bbb68f15d4d9f20f85ee4d17c42f9cac6
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-03-18 15:36:48 +0900

    [level 3] Title: 입국심사, Time: 839.11 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,18 +1,14 @@
 def solution(n, times):
-    left = 1
-    right = max(times)*n
-    answer = 0
+    times.sort()
+    left, right = 0, times[0]*n
     while left <= right:
-        people = 0
-        mid = (left+right)//2
-        for time in times:
-            people += mid//time
-            if people >= n:
-                break
-        if people >= n:
-            answer = mid
+        mid = (left + right)//2
+        cnt = 0
+        for t in times:
+            cnt += mid//t
+        #print(cnt, mid)
+        if cnt >= n: #사람 수가 많으면 -> 시간 줄이기
             right = mid - 1
-        else :
-            left = mid + 1
-    
-    return answer
+        else:
+            left = mid + 1    
+    return left
