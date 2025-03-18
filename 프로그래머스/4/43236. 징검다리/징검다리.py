b32a957856fe4e97eab97f5fdc7a61d31c489024
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-03-18 16:15:40 +0900

    [level 4] Title: 징검다리, Time: 119.80 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -2,24 +2,21 @@ def solution(distance, rocks, n):
     rocks.sort()
     rocks.append(distance)
     left, right = 1, distance
-    #print(rocks)
     answer = 0
     while left <= right:
         mid = (left + right)//2
-        #print(mid)
-        #print(left, right)
-        cnt = 0
         
+        cnt = 0
         loc = 0
         for stone in rocks:
             dist = stone - loc
-            #print(stone, loc, dist)
             if dist < mid:
                 cnt += 1
                 if cnt > n:
                     break
             else:
                 loc = stone
+        #print(mid, cnt)
         if cnt > n: #너무 많이 지운 경우 -> mid가 너무 높음
             right = mid - 1
         else:
