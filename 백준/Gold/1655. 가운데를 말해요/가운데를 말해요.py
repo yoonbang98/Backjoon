3373fdf7550e20998d3f905bc1793d3403100934
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-03-22 09:09:10 +0900

    [Gold II] Title: 가운데를 말해요, Time: 236 ms, Memory: 37992 KB -BaekjoonHub

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -0,0 +1,23 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+leftHeap = []
+rightHeap = []
+for i in range(N):
+    num = int(input())
+
+    if len(leftHeap) == len(rightHeap):
+        heapq.heappush(leftHeap, -num)
+    else:
+        heapq.heappush(rightHeap, num)
+
+    if rightHeap and rightHeap[0] < -leftHeap[0]:
+        leftValue = heapq.heappop(leftHeap)
+        rightValue = heapq.heappop(rightHeap)
+
+        heapq.heappush(leftHeap, -rightValue)
+        heapq.heappush(rightHeap, -leftValue)
+
+    print(-leftHeap[0])
