c1ce82e9fe644de7e4f96528dcd83fd6331edc87
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-10-13 20:25:04 +0900

    [level 2] Title: 타겟 넘버, Time: 351.82 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,33 +1,16 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
+def dfs(result, n, numbers, target):
+    global answer
+    if n == len(numbers):
+        if result == target:
+            answer += 1
+        return
+    dfs(result + numbers[n], n+1, numbers, target)
+    dfs(result - numbers[n], n+1, numbers, target)
+    return
+        
 answer = 0
-def DFS(idx, numbers, target, value):
-    global answer
-    N = len(numbers)
-    if(idx== N and target == value):
-        answer += 1
-        return
-    if(idx == N):
-        return
-
-    DFS(idx+1,numbers,target,value+numbers[idx])
-    DFS(idx+1,numbers,target,value-numbers[idx])
 def solution(numbers, target):
-    global answer
-    DFS(0,numbers,target,0)
+    dfs(numbers[0], 1, numbers, target)
+    dfs(-numbers[0], 1, numbers, target)
+    
     return answer
