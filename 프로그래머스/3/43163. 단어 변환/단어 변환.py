2b840dfcd6b444ace22598b1fd8f0abb8e29169f
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-10-13 21:05:18 +0900

    [level 3] Title: 단어 변환, Time: 0.44 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,28 +1,25 @@
-from collections import deque
-import copy
+from collections import deque, defaultdict
 def solution(begin, target, words):
-    answer = 1e9
-    N = len(words)
-    word_N = len(begin)
-    visited = [False]*N
+    n = len(begin)
+    if target not in words:
+        return 0
+    visited = defaultdict(int)
+    visited[begin] = 1
     queue = deque()
-    queue.append([begin, 0, visited])
-    
-    while queue :
-        cur_word, result, visited_tmp = queue.popleft()
-        #print(visited_tmp)
-        if cur_word == target:
-            answer = min(answer, result)
-            continue
-        for idx, nxt_word in enumerate(words):
-            if visited_tmp[idx] : continue
-            diff_num = 0
-            for i in range(word_N):
-                if cur_word[i] != nxt_word[i]:
-                    diff_num += 1
-            if diff_num == 1:
-                visited_copy = copy.deepcopy(visited_tmp)
-                visited_copy[idx] = True
-                queue.append([nxt_word, result + 1, visited_copy])
-        
-    return 0 if answer == 1e9 else answer
+    queue.append(begin)
+    while True:
+        new_queue = deque()
+        while queue:
+            cur = queue.popleft()
+            for word in words:
+                cnt = 0
+                for i in range(n):
+                    if word[i] != cur[i]:
+                        cnt += 1
+                if cnt == 1 and not visited[word]:
+                    visited[word] = visited[cur] + 1
+                    new_queue.append(word)
+        if not new_queue: break
+        queue = new_queue
+
+    return visited[target] - 1
