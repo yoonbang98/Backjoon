8529d0caea004367199779fec79e29b5739aaedf
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-10-19 19:36:22 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 16.27 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.py
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.py
@@ -1,93 +1,85 @@
 from collections import deque
+
+dir = [(1,0),(-1,0),(0,1),(0,-1)]
+
+
 def solution(game_board, table):
-    blank_loc_total = []
-    N = len(game_board)
-    visited = [[False]*N for _ in range(N)]
-    dir = [(1,0), (0,1),(-1,0),(0,-1)]
-    for i in range(N):
-        for j in range(N):
-            if not game_board[i][j] and not visited[i][j]:
-                blank_loc = []
-                queue = deque()
-                queue.append([i,j])
-                visited[i][j] = True
-                while queue:
-                    r, c = queue.popleft()
-                    blank_loc.append([r,c])
-                    for dr, dc in dir:
-                        nr, nc = r + dr, c + dc
-                        if 0 <= nr < N and 0 <= nc < N and not game_board[nr][nc] and not visited[nr][nc]:
-                            queue.append([nr,nc])
-                            visited[nr][nc] = True
-                blank_loc_total.append(blank_loc)
-    table_info = []
-    visited = [[False]*N for _ in range(N)]
-    dir = [(1,0), (0,1),(-1,0),(0,-1)]
-    for i in range(N):
-        for j in range(N):
-            if table[i][j] and not visited[i][j]:
-                blank_loc = []
-                queue = deque()
-                queue.append([i,j])
-                visited[i][j] = True
-                while queue:
-                    r, c = queue.popleft()
-                    blank_loc.append([r,c])
-                    for dr, dc in dir:
-                        nr, nc = r + dr, c + dc
-                        if 0 <= nr < N and 0 <= nc < N and table[nr][nc] and not visited[nr][nc]:
-                            queue.append([nr,nc])
-                            visited[nr][nc] = True
-                table_info.append(blank_loc)
-    block_info = [[0]*N for _ in range(N)]
-    for block in table_info:
-        for r, c in block:
-            block_info[r][c] = len(block)
-
-    field_total = []
-    for blank_loc in blank_loc_total:
-        max_r, min_r = max([r for r, _ in blank_loc]), min([r for r, _ in blank_loc])
-        max_c, min_c = max([c for _, c in blank_loc]), min([c for _, c in blank_loc])
-        H, W = max_r - min_r + 1, max_c - min_c + 1
-        field = [[1]*W for _ in range(H)]
-        for r, c in blank_loc:
-            field[r-min_r][c-min_c] = 0
-        field_total.append([field, len(blank_loc)])
     answer = 0
-
-    for field, M in field_total:
-        flag3 = True
-        #print(field, answer, table, M)
-        for _ in range(4):
-            field = list(map(list, zip(*field[::-1])))
-            H, W = len(field), len(field[0])
-            flag2 = True
-            for sr in range(N-H+1):
-                for sc in range(N-W+1):
-                    tmp_loc = []
-                    flag = True
-                    for r in range(H):
-                        for c in range(W):
-                            if field[r][c] + table[sr+r][sc+c] == 1:
-                                if field[r][c] == 0 and table[sr+r][sc+c] == 1 and M == block_info[sr+r][sc+c]:
-                                    tmp_loc.append([sr+r, sc+c])
-                                elif field[r][c] == 0 and table[sr+r][sc+c] == 1 and M != block_info[sr+r][sc+c]:
-                                    flag = False
+    N = len(table)
+    visited = [[-1]*N for _ in range(N)]
+    block_list = []
+    for i in range(N):
+        for j in range(N):
+            if table[i][j] and visited[i][j] == -1:
+                queue = deque()
+                queue.append([i,j])
+                visited[i][j] = True
+                block = []
+                while queue:
+                    cur_r, cur_c = queue.popleft()
+                    block.append([cur_r, cur_c])
+                    for dr, dc in dir:
+                        nr, nc = cur_r + dr, cur_c + dc
+                        if 0 > nr or 0 > nc or N <= nc or N <= nr: continue
+                        if visited[nr][nc] == -1 and table[nr][nc] == 1:
+                            visited[nr][nc] = True
+                            queue.append([nr, nc])
+                num = len(block)
+                min_r, max_r = min([r for r, _ in block]), max([r for r, _ in block])
+                min_c, max_c = min([c for _, c in block]), max([c for _, c in block])
+                tmp = []
+                for r in range(min_r, max_r + 1):
+                    tmp.append(table[r][min_c:max_c + 1])
+                block_list.append([tmp, num])
+    visited = [[-1]*N for _ in range(N)]
+    blank_list = []
+    for i in range(N):
+        for j in range(N):
+            if not game_board[i][j] and visited[i][j] == -1:
+                queue = deque()
+                queue.append([i,j])
+                visited[i][j] = True
+                block = []
+                while queue:
+                    cur_r, cur_c = queue.popleft()
+                    block.append([cur_r, cur_c])
+                    for dr, dc in dir:
+                        nr, nc = cur_r + dr, cur_c + dc
+                        if 0 > nr or 0 > nc or N <= nc or N <= nr: continue
+                        if visited[nr][nc] == -1 and not game_board[nr][nc]:
+                            visited[nr][nc] = True
+                            queue.append([nr, nc])
+                num = len(block)
+                min_r, max_r = min([r for r, _ in block]), max([r for r, _ in block])
+                min_c, max_c = min([c for _, c in block]), max([c for _, c in block])
+                tmp = []
+                for r in range(min_r, max_r + 1):
+                    tmp.append(game_board[r][min_c:max_c + 1])
+                blank_list.append([tmp, num])
+    visited2 = [False]*len(block_list)
+    for blank, blank_num in blank_list:
+        flag = False
+        for idx, (block, block_num) in enumerate(block_list):
+            if flag : break
+            if not visited2[idx] and blank_num == block_num:
+                for _ in range(4):
+                    if flag : break
+                    block = list(map(list, zip(*block[::-1])))
+                    if len(block) == len(blank) and len(block[0]) == len(blank[0]):
+                        flag2 = True
+                        for r in range(len(block)):
+                            for c in range(len(block[0])):
+                                if not block[r][c] + blank[r][c] == 1:
+                                    flag2 = False
                                     break
-                            else: 
-                                flag = False
-                                break
-                        if not flag:
-                            break
-                    if flag: 
-                        for rr, cc in tmp_loc:
-                            table[rr][cc] = 0
-                        answer += len(tmp_loc)
-                        flag2 = False
-                        break
-                if not flag2: 
-                    flag3 = False
-                    break
-            if not flag3: break
-    
+                            if not flag2 : break
+                        if flag2 : 
+                            flag = True
+                            visited2[idx] = True
+                    else:
+                        continue
+    for idx, v in enumerate(visited2):
+        if v :
+            answer += block_list[idx][1]
+            
     return answer
