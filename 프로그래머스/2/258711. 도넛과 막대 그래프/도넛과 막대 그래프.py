c82a6c280c983f5a52adec45aabcf78cb62183fa
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-10-24 21:51:43 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1379.32 ms, Memory: 163 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.py
@@ -1,30 +1,40 @@
+from collections import deque
 def solution(edges):
     N = 0
-    answer = [0,0,0,0]
+    answer = []
     for src, dst in edges:
         N = max(N, src, dst)
-    exist = [False]*(N+1)
-    edge_list = [[] for _ in range(N+1)]
+    graph = [[] for _ in range(N+1)]
     indegree = [0]*(N+1)
     for src, dst in edges:
-        edge_list[src].append(dst)
-        exist[src] = True
-        exist[dst] = True
+        graph[src].append(dst)
         indegree[dst] += 1
-    gen = 0
-    gen_out = 0
-    for n in range(1,N+1):
-        if exist[n] and not indegree[n] and gen_out < len(edge_list[n]):
-            gen_out = len(edge_list[n])
-            gen = n
-    answer[0] = gen
-    for start in edge_list[gen]:
-        indegree[start] -= 1
-    for start in range(1,N+1):
-        if not exist[start] : continue
-        if indegree[start] == 2:
-            answer[3] += 1
-        if not indegree[start] and start != gen:
+    max_idx, max_num = 0, 0
+    for idx, nei_list in enumerate(graph):
+        if not idx : continue
+        if len(nei_list) >= max_num and not indegree[idx]:
+            max_num = len(nei_list)
+            max_idx = idx
+    answer.append(max_idx)
+    answer.extend([0,0,0])
+    visited = [False]*(N+1)
+    for start in graph[max_idx]:
+        queue = deque()
+        queue.append(start)
+        visited[start] = True
+        num_edge, num_node = 0, 0
+        while queue:
+            cur = queue.popleft()
+            num_edge += len(graph[cur])
+            num_node += 1
+            for nei in graph[cur]:
+                if not visited[nei]:
+                    queue.append(nei)
+                    visited[nei] = True
+        if num_edge == num_node:
+            answer[1] += 1
+        elif num_edge == num_node - 1:
             answer[2] += 1
-    answer[1] = len(edge_list[gen]) - answer[2] - answer[3]
+        else:
+            answer[3] += 1
     return answer
