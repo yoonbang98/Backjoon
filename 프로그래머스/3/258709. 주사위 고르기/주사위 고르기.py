6f28aac731a4306c18aea6e9144025f8bb797e61
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-10-25 21:57:03 +0900

    [level 3] Title: 주사위 고르기, Time: 9982.66 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,32 +1,27 @@
 from itertools import combinations, product
-from collections import defaultdict
 def solution(dice):
     N = len(dice)
     answer = []
     visited = []
-    for com in combinations([i for i in range(N)], int(N/2)):
-        if list(com) in visited: continue
-        visited.append(list(com))
-        counter_dice = list(set([i for i in range(N)])-set(com))
-        dice_list = []
-        for n in com:
-            dice_list.append(dice[n])
-        counter_dice_list = []
-        for n in counter_dice:
-            counter_dice_list.append(dice[n])
-        visited.append(counter_dice)    
+    max_num = 0
+    for a_com in combinations([i for i in range(N)], int(N/2)):
+        b_com = list(set([i for i in range(N)]) - set(a_com))
+        if a_com in visited : continue
+        visited.append(list(a_com))
+        visited.append(b_com)
+        a_dice = [dice[a] for a in a_com]
+        b_dice = [dice[b] for b in b_com]
         result = [0]*501
-        for per in product(*dice_list):
-            result[sum(per)] += 1
+        for pro in product(*a_dice):
+            result[sum(pro)] += 1
         win_number, lose_number = 0, 0
-        for per in product(*counter_dice_list):
-            win_number += sum(result[sum(per)+1:])
-            lose_number += sum(result[:sum(per)])
-        answer.append(list(com) + [win_number])
-        answer.append(counter_dice + [lose_number])
-
-    answer.sort(key = lambda x : -x[-1])
-    answer = answer[0][:-1]
-    for idx, a in enumerate(answer):
-        answer[idx] += 1
-    return answer
+        for pro2 in product(*b_dice):
+            win_number += sum(result[sum(pro2) + 1:])
+            lose_number += sum(result[:sum(pro2)])
+        answer.append([win_number, a_com])
+        answer.append([lose_number, b_com])
+    answer.sort(key = lambda x : -x[0])
+    answer_final = []
+    for a in answer[0][1]:
+        answer_final.append(a+1)
+    return answer_final
