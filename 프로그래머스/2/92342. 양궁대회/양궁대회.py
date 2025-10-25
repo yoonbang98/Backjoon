8dd45fc7ba198aa64d6e7767ef7dea01c8d7223a
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-10-25 14:36:06 +0900

    [level 2] Title: 양궁대회, Time: 1176.09 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -1,49 +1,24 @@
+from itertools import combinations_with_replacement
 def solution(n, info):
-
-    def brut(n, lion, idx): # 남은 화살수, 과녁, 현재 idx
-        nonlocal gap, answer
-
-        if idx > 10: # 정지조건
-            # 화살이 남은경우 마지막에 다쏜다고 가정
-            if n > 0:
-                lion[10] += n
-            score_apeach = 0
-            score_lion = 0
-            # 점수 세기
-            for idx in range(0,11):
-                if lion[idx] > apeach[idx]:
-                    score_lion += 10-idx
-                elif apeach[idx]:
-                    score_apeach += 10-idx
-
-            if score_lion - score_apeach > gap:
-                gap = score_lion - score_apeach
-                answer = lion[:]
-
-            # 낮은 점수를 많이 쏜 경우 정답 최신화
-            elif score_lion - score_apeach == gap:
-                for i in range(10,-1,-1):
-                    if answer[i] < lion[i]:
-                        answer = lion[:]
-                        break
-                    elif answer[i] > lion[i]:
-                        break
-            lion[10] -= n
-            return
-
-        # 이번 idx(점수) 어피치를 이기는 경우와 지는경우 모두 탐색
-        ## 이기는 경우
-        if apeach[idx] + 1 <= n:
-            lion[idx] = apeach[idx] + 1
-            brut(n-apeach[idx]-1, lion, idx+1)
-            lion[idx] = 0
-        ## 지는 경우
-        brut(n, lion, idx+1)
-
-    apeach = info
-    lion = [0] * 11
-    gap = 1
-    answer = []
-    brut(n, lion, 0)
-
-    return answer if answer else [-1]
+    result = []
+    for com in combinations_with_replacement([i for i in range(11)], n):
+        tmp = [0]*11
+        l_score, a_score = 0, 0
+        for c in com:
+            tmp[c] += 1
+        for i in range(11):
+            if info[i] >= tmp[i] and info[i]:
+                a_score += 10 - i
+            elif info[i] < tmp[i] and tmp[i]:
+                l_score += 10 - i
+        if l_score > a_score:
+            tmp = "".join(str(s) for s in tmp)[::-1]
+            result.append([l_score-a_score, tmp])
+    if not result:
+        return [-1]
+    else:
+        result.sort(key = lambda x : (-x[0], -int(x[1])))
+        answer = []
+        for r in result[0][1]:
+            answer.append(int(r))
+        return answer[::-1]
