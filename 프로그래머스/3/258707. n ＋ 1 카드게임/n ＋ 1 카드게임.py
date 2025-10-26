364916228118df9241bf01775d88898c0f129ec0
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-10-26 13:24:58 +0900

    [level 3] Title: n + 1 카드게임, Time: 204.35 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
@@ -1,33 +1,30 @@
-from itertools import combinations
 from collections import deque
 
-def play_game(card_deck, card_deck2, N):
-    card_deck2 = set(card_deck2)
-    for card in card_deck:
+def next_round(card_deck1, card_deck2, N):
+    for card in card_deck1:
         if N + 1 - card in card_deck2:
+            card_deck1.remove(card)
             card_deck2.remove(N+1-card)
-            card_deck.remove(card)
             return True
     return False
-    
 def solution(coin, cards):
-    answer = 0
     N = len(cards)
-    card_deck = set(cards[:int(N/3)])
-    card_remainder = deque(cards[int(N/3):])
+    card_deck = cards[:int(N/3)]
+    remainder = deque(cards[int(N/3):])
     pending = []
-    turn = 1
-    while coin >= 0 and card_remainder:
-        pending.append(card_remainder.popleft())
-        pending.append(card_remainder.popleft())
+    answer = 1
     
-        if play_game(card_deck, card_deck, N):
+    while coin >= 0 and remainder:
+        pending.append(remainder.popleft())
+        pending.append(remainder.popleft())
+        
+        if next_round(card_deck, card_deck, N):
             pass
-        elif coin >= 1 and play_game(card_deck, pending, N):
+        elif coin >= 1 and next_round(card_deck, pending, N):
             coin -= 1
-        elif coin >= 2 and play_game(pending, pending, N):
+        elif coin >= 2 and next_round(pending, pending, N):
             coin -= 2
         else:
             break
-        turn += 1
-    return turn
+        answer += 1
+    return answer
