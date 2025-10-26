2cbac851b5440fa584db2e2b9a70c2f6f32de5ae
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-10-26 22:13:17 +0900

    [level 3] Title: 등산코스 정하기, Time: 952.75 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.py
@@ -1,41 +1,31 @@
 import heapq
 from math import inf
-
 def solution(n, paths, gates, summits):
-    # 간선 정리 (양방향)
-    graph = [[] for _ in range(n + 1)]
-    for i, j, w in paths:
-        graph[i].append([j, w])
-        graph[j].append([i, w])
 
-    # 산봉우리 판별
-    is_summit = [False] * (n + 1)
+    graph = [[] for _ in range(n+1)]
+    for src, dst, w in paths:
+        graph[src].append([dst, w])
+        graph[dst].append([src, w])
+    
+    is_summits = [0]*(n+1)
     for summit in summits:
-        is_summit[summit] = True
-
-    # gates 모두 시작 위치
-    distance = [inf] * (n + 1)
-    queue = []
+        is_summits[summit] = True
+    heap = []
+    distance = [inf]*(n+1)
     for gate in gates:
+        heapq.heappush(heap, [0, gate])
         distance[gate] = 0
-        heapq.heappush(queue, [0, gate])
-
-    # 다익스트라
-    while queue:
-        d, i = heapq.heappop(queue)
-        # 산봉우리면 바로 continue
-        # 이렇게 해야 두 개 이상의 산봉우리를 방문하지 않는다.
-        if distance[i] < d or is_summit[i]:
+        
+    while heap:
+        inten, cur = heapq.heappop(heap)
+        
+        if distance[cur] < inten or is_summits[cur]:
             continue
-        for j, dd in graph[i]:
-            dd = max(distance[i], dd)
-            if distance[j] > dd:
-                distance[j] = dd
-                heapq.heappush(queue, [dd, j])
-
-    # 결과
-    # 거리가 같으면 산봉우리의 번호가 작은 것을 출력해야 하므로
-    # 산봉우리를 정렬하여 살펴보자.
+        for nei, weight in graph[cur]:
+            new_inten = max(inten, weight)
+            if distance[nei] > new_inten:
+                heapq.heappush(heap, [new_inten, nei])
+                distance[nei] = new_inten
     result = [-1, inf]
     for summit in sorted(summits):
         if distance[summit] < result[1]:
