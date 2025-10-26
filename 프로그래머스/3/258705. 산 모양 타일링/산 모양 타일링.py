75c222995f9343e120684f57516cdc573724299f
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-10-26 19:51:25 +0900

    [level 3] Title: 산 모양 타일링, Time: 72.11 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
@@ -1,24 +1,13 @@
 def solution(n, tops):
-    dp = []
-    for i in range(2*n + 1):
-        dp.append([0]*3)
-    dp[0][0] = 1
-    dp[0][1] = 1
-    for i in range(2*n+1):
-        if not i :
-            continue
-        if i%2 == 1 : #윗변
-            dp[i][1] = (dp[i-1][0]+dp[i-1][2])%10007 
-            if tops[i//2]: #위에 놓여져 있는 경우
-                dp[i][0] = (sum(dp[i-1]))%10007
-                dp[i][2] = (dp[i-1][0]+dp[i-1][2])%10007
-            else:
-                dp[i][0] = (dp[i-1][0]+dp[i-1][2])%10007
-                dp[i][2] = (dp[i-1][1])%10007
-        else : #아랫변
-            dp[i][0] = (dp[i-1][0]+dp[i-1][2])%10007 #작은 삼각형만 놓는 경우
-            dp[i][1] = (dp[i-1][0]+dp[i-1][2])%10007
-            dp[i][2] = (dp[i-1][1])%10007
+    dp_square = [0]*(n+1)
+    dp = [0]*(n+1)
+    dp[0] = 1
+    for i in range(n):
+        if tops[i] : #위에 삼각형이 있을 때
+            dp_square[i+1] = (dp_square[i] + dp[i])%10007
+            dp[i+1] = (3*dp[i] + 2*dp_square[i])%10007
+        else: #위에 삼각형이 없을 때
+            dp_square[i+1] = (dp_square[i] + dp[i])%10007
+            dp[i+1] = (2*dp[i] + dp_square[i])%10007
 
-
-    return (dp[-1][0] + dp[-1][2])%10007
+    return sum(dp)%10007
