f47994916b2069327491610f8287bd5b08067fa8
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-10-28 20:52:28 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 812.97 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -1,31 +1,25 @@
-from collections import deque
-
+import heapq
+from math import inf
 def solution(alp, cop, problems):
-    # 목표 alp, cop 찾기
-    max_alp, max_cop = alp, cop
+    dst_alp, dst_cop = alp, cop
     for a_req, c_req, _, _, _ in problems:
-        max_alp = max(max_alp, a_req)
-        max_cop = max(max_cop, c_req)
+        dst_alp = max(dst_alp, a_req)
+        dst_cop = max(dst_cop, c_req)
+
+    dist = [[inf]*(dst_cop+1) for _ in range(dst_alp+1)]
+    problems.append([0,0,1,0,1])
+    problems.append([0,0,0,1,1])
+    dist[alp][cop] = 0
+    heap = []
+    heapq.heappush(heap, [0, alp, cop])
     
-    # DP 테이블 초기화
-    INF = float('inf')
-    dp = [[INF] * (max_cop + 1) for _ in range(max_alp + 1)]
-    dp[alp][cop] = 0
+    while heap:
+        cur_cost, cur_r, cur_c = heapq.heappop(heap)
+        for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
+            if alp_req <= cur_r and cop_req <= cur_c :
+                nxt_a, nxt_c = min(dst_alp, cur_r + alp_rwd), min(dst_cop, cur_c + cop_rwd)
+                if dist[nxt_a][nxt_c] > cur_cost + cost:
+                    heapq.heappush(heap, [cur_cost + cost, nxt_a, nxt_c])
+                    dist[nxt_a][nxt_c] = cur_cost + cost
 
-    # 문제 리스트에 기본 공부 방법 추가 (알고력 +1, 코딩력 +0) (코스트 1)
-    problems.append([0, 0, 1, 0, 1])
-    problems.append([0, 0, 0, 1, 1])
-
-    # BFS 탐색
-    queue = deque([(alp, cop)])
-    while queue:
-        a, c = queue.popleft()
-
-        for a_req, c_req, a_rwd, c_rwd, cost in problems:
-            if a >= a_req and c >= c_req:  # 문제를 풀 수 있는 경우
-                nxt_a, nxt_c = min(max_alp, a + a_rwd), min(max_cop, c + c_rwd)
-                if dp[nxt_a][nxt_c] > dp[a][c] + cost:
-                    dp[nxt_a][nxt_c] = dp[a][c] + cost
-                    queue.append((nxt_a, nxt_c))
-
-    return dp[max_alp][max_cop]
+    return dist[dst_alp][dst_cop]
