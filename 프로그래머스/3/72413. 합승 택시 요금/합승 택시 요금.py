743c78d298fc10a6cceb62d8172908055f80cbad
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-10-31 22:39:28 +0900

    [level 3] Title: 합승 택시 요금, Time: 2554.39 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,23 +1,20 @@
 from math import inf
 def solution(n, s, a, b, fares):
-
     answer = 0
-    graph = [[inf]*(n+1) for _ in range(n+1)]
-    for src, dst, fare in fares:
-        graph[src][dst] = fare
-        graph[dst][src] = fare
+    dist = [[inf]*(n+1) for _ in range(n+1)]
+    for c, d, f in fares:
+        dist[c][d] = f
+        dist[d][c] = f
     for i in range(n+1):
-        graph[i][i] = 0
+        dist[i][i] = 0
     for k in range(n+1):
         for i in range(n+1):
             for j in range(n+1):
-                graph[i][j] = min(graph[i][k] + graph[k][j], graph[i][j])
-    threshold = graph[s][a] + graph[s][b]
-    print(threshold)
-    answer = threshold
+                dist[i][j] = min(dist[i][k] + dist[k][j], dist[i][j])
+    answer = dist[s][a] + dist[s][b]
     for mid in range(1, n+1):
         if mid == s : continue
-        distance = graph[s][mid] + graph[mid][a] + graph[mid][b]
+        distance = dist[s][mid] + dist[mid][a] + dist[mid][b]
 
         if answer > distance:
             answer = distance
