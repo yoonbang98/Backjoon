412b01574c338a5a36031d1bcf1e4c972937c910
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2025-11-01 17:34:04 +0900

    [level 3] Title: 징검다리 건너기, Time: 400.20 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,20 +1,22 @@
 def solution(stones, k):
     left, right = 1, 1e9
+    
     while left <= right:
-        mid = (left + right)//2
-        cnt = 0
+        mid = (left + right) // 2
         flag = False
+        cnt = 0 
         for stone in stones:
-            if mid <= stone: #밟을 수 있는 경우
+            if stone >= mid:
                 cnt = 0
-            else : # 밟을 수 없는 경우
+            else:
                 cnt += 1
                 if cnt >= k:
                     flag = True
                     break
-        if flag : #건너지 못하는 경우, mid를 줄여야함
+        if flag:
             right = mid - 1
         else:
-            left = mid + 1   
-                
+            left = mid + 1
+    print(left, right)
+    answer = 0
     return min(left, right)
