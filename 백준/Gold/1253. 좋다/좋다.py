c15d7826537ee9c3ef094eb18c7c6732789c3f80
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2026-02-24 20:50:50 +0900

    [Gold IV] Title: 좋다, Time: 1628 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -1,23 +1,25 @@
-N = int(input())
-num_list = list(map(int, input().split()))
-num_list.sort()
-answer = 0
+import sys
 
-for idx, num in enumerate(num_list):
-    left, right = 0, N-1
-    while left < right:
-        if left == idx:
-            left += 1
-            continue
-        if right == idx:
-            right -= 1
-            continue
-        result = num_list[left] + num_list[right]
-        if result <= num :
-            if result == num:
-                answer += 1
-                break
-            left += 1
+N = int(sys.stdin.readline())
+num_list = list(map(int, sys.stdin.readline().split()))
+num_list.sort()
+
+answer = 0
+for i in range(N):
+    num_list_tmp = []
+    for idx, num in enumerate(num_list):
+        if i != idx :
+            num_list_tmp.append(num)
         else:
+            target = num
+    left, right = 0, N-2
+    while left < right:
+        val = num_list_tmp[left] + num_list_tmp[right]
+        if val < target:
+            left += 1
+        elif val > target:
             right -= 1
+        else:
+            answer += 1
+            break
 print(answer)
