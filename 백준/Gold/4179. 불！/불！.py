524bff9d7866481cc3c023d19442cdf88c902ab9
YunByeongChae <121426255+yoonbang98@users.noreply.github.com>
2026-03-01 14:50:25 +0900

    [Gold III] Title: 불!, Time: 1152 ms, Memory: 40452 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -1,58 +1,43 @@
-from collections import deque
 import sys
-input = sys.stdin.readline
-R, C = map(int, input().split())
-field = [list(input()) for _ in range(R)]
-j_r, j_c = -1, -1
+from collections import deque
+R, C = map(int, sys.stdin.readline().split())
+field = []
 
-fire_loc = []
-for i in range(R):
-    for j in range(C):
-        if field[i][j] == 'J':
-            j_r, j_c = i, j
-        if field[i][j] == 'F':
-            fire_loc.append([i,j])
-answer = 1e9
-dir = [(1,0),(0,1),(-1,0),(0,-1)]
-visited = [[-1]*C for _ in range(R)]
-visited[j_r][j_c] = 0
+fire = []
+jihun = []
+for r in range(R):
+    row = list(sys.stdin.readline().strip())
+    for c in range(C):
+        if row[c] == 'J':
+            jihun.append([r,c])
+        if row[c] == 'F':
+            fire.append([r,c])
+    field.append(row)
+
+dir = [(1,0),(0,1),(0,-1),(-1,0)]
+
 queue = deque()
-queue.append([j_r, j_c, 0])
-fire_queue = deque(fire_loc)
+queue.append((jihun[0][0], jihun[0][1], "J"))
+field[jihun[0][0]][jihun[0][1]] = 0
 
-while True:
-    nxt = []
+for r,c in fire:
+    queue.append((r,c,"F"))
+    field[r][c] = "#"
+def bfs():
     while queue:
-        r,c, dist = queue.popleft()
+        r,c,case = queue.popleft()
+        if case == 'J' and (r == 0 or r == R-1 or c == 0 or c == C-1):
+            if field[r][c] == '#':
+                continue
+            return field[r][c] + 1
         for dr, dc in dir:
             nr, nc = r + dr, c + dc
-            if field[r][c] == 'F': continue
-            if 0 <= nr < R and 0 <= nc < C and visited[nr][nc] == -1 and field[nr][nc] =='.':
-                visited[nr][nc] = dist + 1
-                field[nr][nc] = 'J'
-                nxt.append([nr,nc, visited[nr][nc]])
-            if 0 > nr or R <= nr or 0 > nc or C <= nc:
-                answer = dist + 1
-                break
-    queue = deque(nxt)
-    if answer != 1e9:
-        break
-    if not queue:
-        answer = -1
-        break
-    nxt_fire = []
-
-    while fire_queue:
-        fr, fc = fire_queue.popleft()
-        for dr, dc in dir:
-            f_nr, f_nc = dr + fr, dc + fc
-
-            if 0 <= f_nr < R and 0 <= f_nc < C and (field[f_nr][f_nc] == 'J' or field[f_nr][f_nc] == '.'):
-                field[f_nr][f_nc] = 'F'
-                nxt_fire.append([f_nr, f_nc])
-    fire_queue = deque(nxt_fire)
-
-if answer == -1:
-    print('IMPOSSIBLE')
-else:
-    print(answer)
+            if 0 <= nr < R and 0 <= nc < C:
+                if field[nr][nc] != '#' and case == 'F':
+                    field[nr][nc] = '#'
+                    queue.append((nr,nc, 'F'))
+                elif field[nr][nc] == '.' and case == 'J' and field[r][c] != '#':
+                    field[nr][nc] = field[r][c] + 1
+                    queue.append((nr,nc, 'J'))
+    return 'IMPOSSIBLE'
+print(bfs())
